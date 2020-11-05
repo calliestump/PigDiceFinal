95cacde8cfd31f39d65cf6e06bc6f6fb8ccf1ce0
Callie Stump <callie@stu.mp>
2020-11-05 14:17:03 -0500

    improves functionality for objects
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 // Business Logic ----- >
-function Player(name, turnTotal, diceRoll, overallScore) {
+function Player(name) {
   this.name = name;
   this.diceRoll = 0;
   this.turnTotal = 0;
   this.overallScore = 0;
+  this.currentWins = 0;
 }
 //Creates Rolling variable and conditiional statement for "1"
 Player.prototype.roll = function() {
@@ -22,9 +23,11 @@ Player.prototype.roll = function() {
     }
     return alert("You got a 1 :( Next players turn");
   }
-  if (this.diceRoll + this.turnTotal + this.overallScroe >= 10) {
+  if ((this.diceRoll + this.turnTotal + this.overallScore) >= 10) {
     alert("Game Over. You win!")
-    $(".reset").show();
+    console.log("We're getting into conditional code block at bottom of .roll function call");
+    
+    this.currentWins++;
   }
   this.turnTotal += this.diceRoll;
 }
@@ -34,8 +37,15 @@ Player.prototype.hold = function () {
 }
 Player.prototype.scoreCheck = function() {
   if (this.overallScore >= 10) {
+    this.currentWins++;
+    //$(".turnScore1").val();
     alert("Game Over. You win!")
-    $(".reset").show();
+    $(".resetButton").show();
+    $(".result").show();
+  } else if (($(".turnScore1").val() + $(".turnScore2").val()) >= 10) {
+    alert("Game Over. Our jQueries are telling us you've won!")
+    $(".resetButton").show();
+    $(".result").show();
   } else {
     return false;
   }
@@ -50,9 +60,11 @@ $(document).ready(function() {
     // Objects
     player1 = new Player(gamer1);
     player2 = new Player(gamer2);
-
     $(".player1NameOutput").text(player1.name);
     $(".player2NameOutput").text(player2.name);
+    // span class="player2WinTotal"
+    $("span.player2WinTotal").text(player2.currentWins);
+    $("span.player1WinTotal").text(player1.currentWins);
   });
   
   $(".roll1").click(function(event) {
@@ -63,6 +75,7 @@ $(document).ready(function() {
     $(".diceRoll1").text(player1.diceRoll);
     $(".turnScore1").text(player1.turnTotal);
     player1.scoreCheck();
+    $(".overallScore1").text(player1.overallScore);
   });
   $(".roll2").click(function(event) {
     event.preventDefault();
@@ -72,6 +85,7 @@ $(document).ready(function() {
     $(".diceRoll2").text(player2.diceRoll);
     $(".turnScore2").text(player2.turnTotal);
     player2.scoreCheck();
+    $(".overallScore2").text(player2.overallScore);
   });
 
   $(".hold1").click(function(event) {
@@ -82,7 +96,7 @@ $(document).ready(function() {
     player1.diceRoll = 0;
     player1.turnTotal = 0;
     $(".diceRoll1").text(player1.diceRoll);
-    $(".turnScore2").text(player1.turnTotal);
+    $(".turnScore1").text(player1.turnTotal);
     $("div.player1Box").addClass("not-active");
     $("div.player2Box").removeClass("not-active");
   });
